0d55dadcd39063e19e080a20ae068dc8a31447e8
doksii <127222249+doksii@users.noreply.github.com>
2023-05-19 21:30:52 +0800

    Add files via upload

---
+++ Finals/assets/js/Finals.js
@@ -0,0 +1,26 @@
+function home() 
+{
+const element = document.getElementById("home");
+element.scrollIntoView();
+}
+function skills() 
+{
+const element = document.getElementById("skills");
+element.scrollIntoView();
+}
+function credentials() 
+{
+const element = document.getElementById("credentials");
+element.scrollIntoView();
+}
+function contacts() 
+{
+const element = document.getElementById("contacts");
+element.scrollIntoView();
+}
+function about() 
+{
+const element = document.getElementById("about");
+element.scrollIntoView();
+}
+
